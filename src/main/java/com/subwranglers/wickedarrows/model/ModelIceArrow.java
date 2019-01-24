158efe7540f40b82e20514f1cae838fb53ea635c
Todd S <todd.scharien@gmail.com>
2019-01-24 11:38:54 -0800

    render ice arrow entity model

--- src/main/java/com/subwranglers/wickedarrows/model/ModelIceArrow.java
+++ src/main/java/com/subwranglers/wickedarrows/model/ModelIceArrow.java
@@ -1,110 +1,41 @@
-package com.subwranglers.wickedarrows.model;// Cubik Studio 2.9.480 Beta JAVA exporter
-// Model generated using MrCrayfish's Model Creator (http://mrcrayfish.com/modelcreator/)
+package com.subwranglers.wickedarrows.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
 public class ModelIceArrow extends ModelBase {
+    private final ModelRenderer arrow;
 
-    //fields
-    public ModelRenderer e1;
-    public ModelRenderer e2;
-    public ModelRenderer e3;
-    public ModelRenderer e4;
-    public ModelRenderer e5;
-    public ModelRenderer e6;
-    public ModelRenderer e7;
-    public ModelRenderer e8;
-    public ModelRenderer e9;
+    public ModelIceArrow() {
+        textureWidth = 32;
+        textureHeight = 32;
 
-    public ModelIceArrow()
-    {
-        textureWidth = 64;
-        textureHeight = 64;
-
-        e1 = new ModelRenderer(this, 24, 5);
-        e1.setRotationPoint(-8.5F, 8F, -8F);
-        e1.addBox(0F, 0F, 0F, 1, 10, 1);
-        e1.setTextureSize(64, 64);
-        e1.mirror = false;
-        setRotation(e1, 0F, 0F, 0F);
-        e2 = new ModelRenderer(this, 0, 4);
-        e2.setRotationPoint(-11F, 22F, -11F);
-        e2.addBox(0F, 0F, 0F, 6, 6, 6);
-        e2.setTextureSize(64, 64);
-        e2.mirror = false;
-        setRotation(e2, 0F, 0F, 0F);
-        e3 = new ModelRenderer(this, 0, 0);
-        e3.setRotationPoint(-9F, 16F, -8F);
-        e3.addBox(0F, 4.768372E-07F, 0F, 2, 3, 1);
-        e3.setTextureSize(64, 64);
-        e3.mirror = false;
-        setRotation(e3, 0F, 0F, 0F);
-        e4 = new ModelRenderer(this, 6, 0);
-        e4.setRotationPoint(-7F, 15F, -8F);
-        e4.addBox(0F, 1.430511E-06F, 4.768372E-07F, 1, 3, 1);
-        e4.setTextureSize(64, 64);
-        e4.mirror = false;
-        setRotation(e4, 0F, 0F, 0F);
-        e5 = new ModelRenderer(this, 24, 0);
-        e5.setRotationPoint(-8F, 16F, -9F);
-        e5.addBox(0F, 1.430511E-06F, 0F, 1, 3, 2);
-        e5.setTextureSize(64, 64);
-        e5.mirror = false;
-        setRotation(e5, 0F, 0F, 0F);
-        e6 = new ModelRenderer(this, 10, 0);
-        e6.setRotationPoint(-8F, 15F, -10F);
-        e6.addBox(0F, 1.430511E-06F, 0F, 1, 3, 1);
-        e6.setTextureSize(64, 64);
-        e6.mirror = false;
-        setRotation(e6, 0F, 0F, 0F);
-        e7 = new ModelRenderer(this, 10, 0);
-        e7.setRotationPoint(-8F, 15F, -7.000001F);
-        e7.addBox(0F, 1.430511E-06F, 9.536743E-07F, 1, 3, 1);
-        e7.setTextureSize(64, 64);
-        e7.mirror = false;
-        setRotation(e7, 0F, 0F, 0F);
-        e8 = new ModelRenderer(this, 6, 0);
-        e8.setRotationPoint(-10F, 15F, -8F);
-        e8.addBox(-4.768372E-07F, 1.430511E-06F, 4.768372E-07F, 1, 3, 1);
-        e8.setTextureSize(64, 64);
-        e8.mirror = false;
-        setRotation(e8, 0F, 0F, 0F);
-        e9 = new ModelRenderer(this, 28, 5);
-        e9.setRotationPoint(-8F, 8F, -8.5F);
-        e9.addBox(0F, 0F, 0F, 1, 10, 1);
-        e9.setTextureSize(64, 64);
-        e9.mirror = false;
-        setRotation(e9, 0F, 0F, 0F);
+        arrow = new ModelRenderer(this);
+        arrow.setRotationPoint(0.0F, 24.0F, 0.0F);
+        setRotationAngle(arrow, -1.5708F, -1.5708F, 0.0F);
+        arrow.cubeList.add(new ModelBox(arrow, 0, 0, -2.0F, 3.0F, -26.0F, 4, 4, 4, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 4, 10, 1.5F, -7.0F, -24.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 4, 9, 0.5F, -6.0F, -24.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 3, 8, -2.5F, -7.0F, -24.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 4, 8, -1.5F, -6.0F, -24.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 4, 10, 0.0F, -6.0F, -25.5F, 0, 3, 1, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 3, 10, 0.0F, -7.0F, -26.5F, 0, 3, 1, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 4, 7, 0.0F, -7.0F, -22.5F, 0, 3, 1, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 3, 7, 0.0F, -6.0F, -23.5F, 0, 3, 1, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 8, -0.5F, -7.0F, -24.0F, 1, 10, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 8, 0.0F, -7.0F, -24.5F, 0, 10, 1, 0.0F, false));
     }
 
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-    {
-        super.render(entity, f, f1, f2, f3, f4, f5);
-        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-
-        e1.render(f5);
-        e2.render(f5);
-        e3.render(f5);
-        e4.render(f5);
-        e5.render(f5);
-        e6.render(f5);
-        e7.render(f5);
-        e8.render(f5);
-        e9.render(f5);
+    @Override
+    public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        arrow.render(scale);
     }
 
-    private void setRotation(ModelRenderer model, float x, float y, float z)
-    {
-        model.rotateAngleX = x;
-        model.rotateAngleY = y;
-        model.rotateAngleZ = z;
+    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
     }
-     
-    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-    {
-        super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    }
- 
 }
--- src/main/java/com/subwranglers/wickedarrows/render/RenderIceArrow.java
+++ src/main/java/com/subwranglers/wickedarrows/render/RenderIceArrow.java
@@ -1,27 +1,22 @@
 package com.subwranglers.wickedarrows.render;
 
+import com.subwranglers.wickedarrows.WickedArrows;
 import com.subwranglers.wickedarrows.entity.EntityIceArrow;
 import com.subwranglers.wickedarrows.model.ModelIceArrow;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 import javax.annotation.Nullable;
 
-import static com.subwranglers.wickedarrows.info.Names.QUALIFY;
-import static com.subwranglers.wickedarrows.info.Names.name;
-
 public class RenderIceArrow extends Render<EntityIceArrow> implements IRenderFactory<EntityIceArrow> {
 
-    private static final String ICE_ARROW_TEXTURE = "textures/entity/projectiles/ice_arrow.png";
-    public static final ResourceLocation TEXTURES = new ResourceLocation(name(ICE_ARROW_TEXTURE, QUALIFY));
-    protected ModelBase modelIceArrow = new ModelIceArrow();
+    public static final ResourceLocation TEXTURE = new ResourceLocation(WickedArrows.MOD_ID,
+            "textures/entity/projectiles/ice_arrow.png");
+
+    protected ModelIceArrow modelIceArrow = new ModelIceArrow();
 
     public RenderIceArrow(RenderManager renderManagerIn) {
         super(renderManagerIn);
@@ -30,39 +25,60 @@ public class RenderIceArrow extends Render<EntityIceArrow> implements IRenderFac
     @Override
     public void doRender(EntityIceArrow entity, double x, double y, double z, float entityYaw, float partialTicks) {
         this.bindEntityTexture(entity);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
         GlStateManager.pushMatrix();
         GlStateManager.disableLighting();
+
         GlStateManager.translate((float) x, (float) y, (float) z);
-        GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.0F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks, 0.0F, 0.0F, 1.0F);
-//        Tessellator tessellator = Tessellator.getInstance();
-//        BufferBuilder bufferbuilder = tessellator.getBuffer();
 
-        GlStateManager.enableRescaleNormal();
-        float f9 = (float) entity.arrowShake - partialTicks;
+        GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.f, 0.f, 1.f, 0.f);
+        GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks, 0.f, 0.f, 1.f);
 
-        if (f9 > 0.0F) {
-            float f10 = -MathHelper.sin(f9 * 3.0F) * f9;
-            GlStateManager.rotate(f10, 0.0F, 0.0F, 1.0F);
-        }
+        modelIceArrow.render(entity, 0, 0, partialTicks, entity.rotationYaw, entity.rotationPitch, .07f);
 
-        GlStateManager.rotate(45.0F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.scale(0.05625F, 0.05625F, 0.05625F);
-        GlStateManager.translate(-4.0F, 0.0F, 0.0F);
-
-        modelIceArrow.render(entity, 0, 0, 0, 0, 0, 1);
-
-        GlStateManager.disableRescaleNormal();
         GlStateManager.enableLighting();
         GlStateManager.popMatrix();
+
         super.doRender(entity, x, y, z, entityYaw, partialTicks);
+
+//        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+//        GlStateManager.pushMatrix();
+//        GlStateManager.disableLighting();
+//        GlStateManager.translate((float) x, (float) y, (float) z);
+//        GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.0F, 0.0F, 1.0F, 0.0F);
+//        GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks, 0.0F, 0.0F, 1.0F);
+//        GlStateManager.enableRescaleNormal();
+//        float f9 = (float) entity.arrowShake - partialTicks;
+//
+//        if (f9 > 0.0F) {
+//            float f10 = -MathHelper.sin(f9 * 3.0F) * f9;
+//            GlStateManager.rotate(f10, 0.0F, 0.0F, 1.0F);
+//        }
+//
+//        GlStateManager.rotate(45.0F, 1.0F, 0.0F, 0.0F);
+//        GlStateManager.scale(0.05625F, 0.05625F, 0.05625F);
+//        GlStateManager.translate(-4.0F, 0.0F, 0.0F);
+//
+//        if (this.renderOutlines) {
+//            GlStateManager.enableColorMaterial();
+//            GlStateManager.enableOutlineMode(this.getTeamColor(entity));
+//        }
+//
+//        modelIceArrow.render(entity, 0, 0, partialTicks, entity.rotationYaw, entity.rotationPitch, 1.f);
+//        if (this.renderOutlines) {
+//            GlStateManager.disableOutlineMode();
+//            GlStateManager.disableColorMaterial();
+//        }
+//
+//        GlStateManager.disableRescaleNormal();
+//        GlStateManager.enableLighting();
+//        GlStateManager.popMatrix();
     }
 
     @Nullable
     @Override
     protected ResourceLocation getEntityTexture(EntityIceArrow entity) {
-        return TEXTURES;
+        return TEXTURE;
     }
 
     @Override
