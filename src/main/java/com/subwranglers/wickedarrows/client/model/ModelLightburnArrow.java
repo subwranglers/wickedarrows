f22a1b00c475b16d0ddee8b5a9320bb423e4c4da
Todd S <todd.scharien@gmail.com>
2019-01-25 11:01:50 -0800

    Update lightburn model + texture

--- src/main/java/com/subwranglers/wickedarrows/client/model/ModelLightburnArrow.java
+++ src/main/java/com/subwranglers/wickedarrows/client/model/ModelLightburnArrow.java
@@ -15,27 +15,14 @@ public class ModelLightburnArrow extends ModelBase {
         arrow = new ModelRenderer(this);
         arrow.setRotationPoint(0.0F, 24.0F, 0.0F);
         setRotationAngle(arrow, -1.5708F, -1.5708F, 0.0F);
-        arrow.cubeList.add(new ModelBox(arrow, 0, 16, -0.5F, 6.0F, -26.0F, 1, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 0, 16, 0.0F, 6.0F, -26.5F, 0, 1, 1, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 0, 10, 0.0F, 4.0F, -27.5F, 0, 1, 3, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 0, 14, -1.5F, 4.0F, -26.0F, 3, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 13, 0.5F, 3.0F, -26.0F, 1, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 13, 1.0F, 2.0F, -26.0F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 13, 0.0F, 3.0F, -27.5F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 13, 0.0F, 3.0F, -25.0F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 13, -1.5F, 3.0F, -26.0F, 0, 1, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 11, 0.0F, 3.0F, -27.5F, 0, 4, 3, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 18, -1.5F, 2.0F, -26.0F, 3, 5, 0, 0.0F, false));
         arrow.cubeList.add(new ModelBox(arrow, 0, 0, 0.0F, -8.0F, -26.5F, 0, 12, 1, 0.0F, false));
         arrow.cubeList.add(new ModelBox(arrow, 0, 1, -0.5F, -8.0F, -26.0F, 1, 12, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 0, 0.0F, -8.0F, -27.5F, 0, 2, 1, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 0, 0.5F, -8.0F, -26.0F, 1, 2, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 2, -1.5F, -8.0F, -26.0F, 1, 2, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 1, 0.0F, -8.0F, -25.5F, 0, 2, 1, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 4, 0, 0.0F, -6.0F, -25.5F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 3, 0, 0.5F, -6.0F, -26.0F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 3, 0, 0.0F, -6.0F, -27.0F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 4, 1, -1.0F, -6.0F, -26.0F, 0, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 14, -1.0F, 5.0F, -26.0F, 2, 1, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 2, 12, 0.0F, 5.0F, -27.0F, 0, 1, 2, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 3, 0, 0.0F, -8.0F, -27.5F, 0, 3, 1, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 3, 1, 0.5F, -8.0F, -26.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 2, 1, -1.5F, -8.0F, -26.0F, 1, 3, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 2, 0, 0.0F, -8.0F, -25.5F, 0, 3, 1, 0.0F, false));
     }
 
     @Override
