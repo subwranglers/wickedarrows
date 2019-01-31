e531fd81b532f8abe25ad8bd39afc506a23a95c1
Todd S <todd.scharien@gmail.com>
2019-01-31 12:14:18 -0800

    Adjust SharpArrow model

--- src/main/java/com/subwranglers/wickedarrows/client/model/ModelSharpArrow.java
+++ src/main/java/com/subwranglers/wickedarrows/client/model/ModelSharpArrow.java
@@ -15,15 +15,14 @@ public class ModelSharpArrow extends ModelBase {
         arrow = new ModelRenderer(this);
         arrow.setRotationPoint(0.0F, 24.0F, 0.0F);
         setRotationAngle(arrow, -1.5708F, -1.5708F, 0.0F);
-        arrow.cubeList.add(new ModelBox(arrow, 0, 20, -2.0F, -8.0F, -26.0F, 4, 16, 0, 0.0F, false));
-        arrow.cubeList.add(new ModelBox(arrow, 0, 0, 0.0F, -8.0F, -28.0F, 0, 16, 4, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 20, -2.0F, -12.0F, -24.5F, 4, 16, 0, 0.0F, false));
+        arrow.cubeList.add(new ModelBox(arrow, 0, 0, 0.0F, -12.0F, -26.5F, 0, 16, 4, 0.0F, false));
     }
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
         arrow.render(f5);
     }
-
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
