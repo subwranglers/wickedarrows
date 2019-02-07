636b9c04b2bf3932c9444c0850b186329ab80815
Todd S <todd.scharien@gmail.com>
2019-02-07 15:23:11 -0800

    Add util

---
+++ src/main/java/util/coordinates/AabbUtil.java
@@ -0,0 +1,21 @@
+package util.coordinates;
+
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+
+public class AabbUtil {
+
+    public static AxisAlignedBB getRadiusAabb(double x, double y, double z, double radius) {
+        return new AxisAlignedBB(
+                Math.floor(x) + -radius,
+                Math.floor(y) + -radius,
+                Math.floor(z) + -radius,
+                Math.floor(x) + radius,
+                Math.floor(y) + radius,
+                Math.floor(z) + radius);
+    }
+
+    public static AxisAlignedBB getRadiusAabb(BlockPos pos, double radius) {
+        return getRadiusAabb(pos.getX(), pos.getY(), pos.getZ(), radius);
+    }
+}
